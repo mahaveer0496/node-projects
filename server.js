06981d184a8cc638c014ea0d74b88948f4a17a2b
Mahaveer <mahaveersharma0496@yahoo.com>
2017-05-14 22:05:16 +0530

    using air bnb style guid

--- server.js
+++ server.js
@@ -1,75 +1,65 @@
-/* eslint-env node */
+/* eslint linebreak-style : 0 */
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/cat_app');
+mongoose.connect('mongodb://localhost:27017/yelp_camp');
+mongoose.Promise = global.Promise;
 
-const catSchema = new mongoose.Schema({
-   name: String,
-   age: Number,
-   temperament: String
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
 });
 
-const Cat = mongoose.model('Cat', catSchema);
-
-const george = new Cat({
-   name: 'George',
-   age: 11,
-   temperament: 'angry'
-})
-
-george.save().then((data) => {
-   console.log(data);
-}, (err) => {
-   console.log('ERROR' + err);
-})
-const campgrounds = [{
-      name: 'Teena',
-      image: ' meena'
-   },
-   {
-      name: 'Neena',
-      image: 'sheena'
-   }
-]
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => {
-   res.render('landing', {
-      pageTitle: 'landing page',
-      welcomeMessage: 'Welcome to our Website'
-   })
-})
+  res.render('landing', {
+    pageTitle: 'landing page',
+    welcomeMessage: 'Welcome to our Website',
+  });
+});
 
 app.get('/campgrounds', (req, res) => {
-   res.render('campgrounds', {campgrounds})
-})
+  Campground.find({}).then((campgrounds) => {
+    res.render('campgrounds', {
+      campgrounds,
+    });
+  }, (err) => {
+    console.log(`error : ${err}`);
+  });
+});
 
 app.post('/campgrounds', (req, res) => {
-   const {
-      name,
-      image
-   } = req.body
+  const {
+    name,
+    image,
+  } = req.body;
 
-   campgrounds.push({
-      name,
-      image
-   })
-   res.redirect('/campgrounds')
-})
+  Campground.create({
+    name,
+    image,
+  }).then(() => {
+    res.redirect('/campgrounds');
+  });
+});
 
 app.get('/campgrounds/new', (req, res) => {
-   res.render('new')
-})
+  res.render('new');
+});
 app.get('*', (req, res) => {
-   res.status(404).send('page not found')
-})
+  res.status(404).send('page not found');
+});
 
 app.listen(3000, () => {
-   console.log('app listening on port 3000!')
-})
+  console.log('app listening on port 3000!');
+});
