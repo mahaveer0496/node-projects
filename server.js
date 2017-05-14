5da837af1b677828b626877611c19299697dcb52
Mahaveer <mahaveersharma0496@yahoo.com>
2017-05-14 14:21:35 +0530

    added mongoose to server

--- server.js
+++ server.js
@@ -2,25 +2,70 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/cat_app');
+
+const catSchema = new mongoose.Schema({
+   name: String,
+   age: Number,
+   temperament: String
+});
+
+const Cat = mongoose.model('Cat', catSchema);
+
+const george = new Cat({
+   name: 'George',
+   age: 11,
+   temperament: 'angry'
+})
+
+george.save().then((data) => {
+   console.log(data);
+}, (err) => {
+   console.log('ERROR' + err);
+})
+const campgrounds = [{
+      name: 'Teena',
+      image: ' meena'
+   },
+   {
+      name: 'Neena',
+      image: 'sheena'
+   }
+]
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-   res.render('home', {
-      pageTitle: 'Home page',
+   res.render('landing', {
+      pageTitle: 'landing page',
       welcomeMessage: 'Welcome to our Website'
    })
 })
 
-app.get('/about', (req, res) => {
-   res.render('about', {
-      pageTitle: 'About Page',
-      title: 'tothih'
-   })
+app.get('/campgrounds', (req, res) => {
+   res.render('campgrounds', {campgrounds})
 })
 
+app.post('/campgrounds', (req, res) => {
+   const {
+      name,
+      image
+   } = req.body
+
+   campgrounds.push({
+      name,
+      image
+   })
+   res.redirect('/campgrounds')
+})
+
+app.get('/campgrounds/new', (req, res) => {
+   res.render('new')
+})
 app.get('*', (req, res) => {
    res.status(404).send('page not found')
 })
